fd25f059577381b6826f64177b4ba6c8757eaa6d
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2022-01-19 16:27:10 +0100

    Lazy loading approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,41 +5,7 @@ import { AppComponent } from './app.component';
 import { ThemeModule, ThemeService } from '@dxc-technology/halstack-angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ButtonModule } from './views/button/button.module';
-import { AutosuggestModule } from './views/autosuggest/autosuggest.module';
-import { CheckboxModule } from './views/checkbox/checkbox.module';
-import { DateModule } from './views/date/date.module';
-import { DateInputModule } from './views/date-input/date-input.module';
-import { DialogModule } from './views/dialog/dialog.module';
-import { DropdownModule } from './views/dropdown/dropdown.module';
-import { FileInputModule } from './views/file-input/file-input.module';
-import { HeaderModule } from './views/header/header.module';
-import { HeadingModule } from './views/heading/heading.module';
-import { InputTextModule } from './views/input-text/input-text.module';
-import { FooterModule } from './views/footer/footer.module';
-import { LinkModule } from './views/link/link.module';
-import { NumberInputModule } from './views/number-input/number-input.module';
-import { PaginatorModule } from './views/paginator/paginator.module';
-import { PasswordInputModule } from './views/password-input/password-input.module';
-import { ProgressBarModule } from './views/progress-bar/progress-bar.module';
-import { RadioButtonModule } from './views/radio-button/radio-button.module';
-import { ResultsetTableModule } from './views/resultset-table/resultset-table.module';
-import { SidenavModule } from './views/sidenav/sidenav.module';
-import { SliderModule } from './views/slider/slider.module';
-import { SpinnerModule } from './views/spinner/spinner.module';
-import { SwitchModule } from './views/switch/switch.module';
-import { TabbedSectionModule } from './views/tabbed-section/tabbed-section.module';
-import { TableModule } from './views/table/table.module';
-import { TabsModule } from './views/tabs/tabs.module';
-import { TagModule } from './views/tag/tag.module';
-import { TextInputModule } from './views/text-input/text-input.module';
-import { TextareaModule } from './views/textarea/textarea.module';
-import { ToggleGroupModule } from './views/toggle-group/toggle-group.module';
-import { UplodadModule } from './views/uplodad/uplodad.module';
-import { V3SelectModule } from './views/v3-select/v3-select.module';
-import { V3TextareaModule } from './views/v3-textarea/v3-textarea.module';
-import { WizardModule } from './views/wizard/wizard.module';
-import { SelectModule } from './views/select/select.module';
+import { ButtonModesModule } from './views/button/button-modes/button-modes.module';
 
 @NgModule({
   declarations: [
@@ -48,6 +14,7 @@ import { SelectModule } from './views/select/select.module';
     BrowserModule,
     CommonModule,
     RouterModule,
+    ButtonModesModule,
     AppRoutingModule,
     // ButtonModule,
     // DateModule,
