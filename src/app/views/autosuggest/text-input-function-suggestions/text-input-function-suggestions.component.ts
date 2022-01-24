bdaa08adbfb649a7f50eac8988872b463772b8a5
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2022-01-24 15:23:16 +0100

    Refactoring autosuggest example

--- src/app/views/autosuggest/text-input-function-suggestions/text-input-function-suggestions.component.ts
+++ src/app/views/autosuggest/text-input-function-suggestions/text-input-function-suggestions.component.ts
@@ -1,58 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { of } from 'rxjs';
-import { switchMap, delay } from "rxjs/operators";
+import { switchMap, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './text-input-function-suggestions.component.html',
-  styleUrls: ['./text-input-function-suggestions.component.scss']
+  styleUrls: ['./text-input-function-suggestions.component.scss'],
 })
 export class TextInputFunctionSuggestionsComponent implements OnInit {
-
-  controlledValue = "";
+  controlledValue = '';
 
   options: Array<any> = [
-    "Afghanistan",
-    "Albania",
-    "Algeria",
-    "Andorra",
-    "Angola",
-    "Antigua and Barbuda",
-    "Bahamas",
-    "Bahrain",
-    "Bangladesh",
-    "Barbados",
-    "Cabo Verde",
-    "Cambodia",
-    "Cameroon",
-    "Canada",
-    "Cayman Islands, The",
-    "Central African Republic",
-    "Chad",
-    "Democratic Republic of the Congo",
-    "Dominican Republic",
-    "Dominica",
-    "Denmark",
-    "Djibouti"
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Cabo Verde',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cayman Islands, The',
+    'Central African Republic',
+    'Chad',
+    'Democratic Republic of the Congo',
+    'Dominican Republic',
+    'Dominica',
+    'Denmark',
+    'Djibouti',
   ];
 
   constructor() {
     this.callbackFunc = this.callbackFunc.bind(this);
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onBlur({ value, error }) {
     this.controlledValue = value;
   }
 
-  onChange(value) {
+  onChange({ value, error }) {
     this.controlledValue = value;
   }
 
   callbackFunc(newValue) {
+    console.log(newValue);
     const newOptions = this.options.filter((option) =>
       option.toUpperCase().includes(newValue.toUpperCase())
     );
--- src/app/views/autosuggest/text-input-with-suggestions/text-input-with-suggestions.component.ts
+++ src/app/views/autosuggest/text-input-with-suggestions/text-input-with-suggestions.component.ts
@@ -1,51 +1,47 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './text-input-with-suggestions.component.html',
-  styleUrls: ['./text-input-with-suggestions.component.scss']
+  styleUrls: ['./text-input-with-suggestions.component.scss'],
 })
 export class TextInputWithSuggestionsComponent implements OnInit {
-
-  controlledValue = "";
+  controlledValue = '';
 
   options: Array<any> = [
-    "Afghanistan",
-    "Albania",
-    "Algeria",
-    "Andorra",
-    "Angola",
-    "Antigua and Barbuda",
-    "Bahamas",
-    "Bahrain",
-    "Bangladesh",
-    "Barbados",
-    "Cabo Verde",
-    "Cambodia",
-    "Cameroon",
-    "Canada",
-    "Cayman Islands, The",
-    "Central African Republic",
-    "Chad",
-    "Democratic Republic of the Congo",
-    "Dominican Republic",
-    "Dominica",
-    "Denmark",
-    "Djibouti"
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Cabo Verde',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cayman Islands, The',
+    'Central African Republic',
+    'Chad',
+    'Democratic Republic of the Congo',
+    'Dominican Republic',
+    'Dominica',
+    'Denmark',
+    'Djibouti',
   ];
 
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onBlur({ value, error }) {
     this.controlledValue = value;
   }
 
-  onChange(value) {
+  onChange({ value, error }) {
     this.controlledValue = value;
   }
-
 }
