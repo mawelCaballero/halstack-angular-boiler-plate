5a875eb16364b1f769d894f84aa8e87005718f41
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2022-02-02 12:32:52 +0100

    Removing path starting with src in error.module

--- src/app/views/error/error.module.ts
+++ src/app/views/error/error.module.ts
@@ -1,12 +1,12 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ErrorsHandler } from 'src/app/service/error.service';
 import { ErrorComponent } from './error.component';
 import {
   DxcHeadingModule,
   ThemeModule,
 } from '@dxc-technology/halstack-angular';
 import { CommonModule } from '@angular/common';
+import { ErrorsHandler } from '../../service/error.service';
 
 @NgModule({
   declarations: [ErrorComponent],
